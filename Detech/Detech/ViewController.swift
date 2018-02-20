8bc4e3902a883afc7d19cafab48de30b0919dd2d
Teslim <tsalam3@github.com>
2018-02-20 12:09:21 -0600

    Xcode app for Detech

---
+++ Detech/Detech/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Detech
+//
+//  Created by Teslim on 2/16/18.
+//  Copyright © 2018 Teslim Salami. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+// 1. Setup motion detector, when you have your phone on you and when it is far from you
+// 2. Timer, how long seizure lasts etc...
+// 3. Send emergency alert
+// 4. Geolocation (google maps)
+// 4. Use firebase to store data from user, includes length of user,
+// 5. Create log in
+
