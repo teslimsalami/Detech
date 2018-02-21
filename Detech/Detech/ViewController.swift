a4968f9355c7b04d9222e7dfc9cd32d404ceae60
Teslim <tsalam3@github.com>
2018-02-21 15:35:13 -0600

    ...

--- Detech/Detech/ViewController.swift
+++ Detech/Detech/ViewController.swift
@@ -25,14 +25,28 @@ class ViewController: UIViewController {
 
 }
 
-// 1. Setup motion detector, when you have your phone on you and when it is far from you
-// 2. Timer, how long seizure lasts etc...
+// 1. Setup connection between acceleramotor, when you have your phone on you and when it is far from you.
+// func accelerometerData(from fromDate: Date, to toDate: Date) -> CMSensorDataList?
+
+//  override func motionEnded (_ motion: UIEventSubtype, with event: UIEvent?){
+
+// newBallImage()
+
+//}
+
+
+
+// 2. Timer, data on how long seizure lasts etc...
 // 3. Send emergency alert
 // 4. Geolocation (google maps)
 // 4. Use firebase to store data from user, includes length of user,
 // 5. Create log in
 
 //  override func motionEnded (_ motion: UIEventSubtype, with event: UIEvent?){
+
+
+
+
 
 // newBallImage()
 //}
